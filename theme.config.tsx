a5abc319c4008fbad78538eb6d4969ae9b565764
saber tabatabaei yazdi <saber.tabatabaee@gmail.com>
2025-02-17 16:34:58 +0330

    لوگوی دپنا

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,31 +8,32 @@ const config: DocsThemeConfig = {
   logo: (
     <>
       <img
-        style={{ fontFamily: 'Anjoman', fontSize: '1.5rem', color: 'blue' , width: "80px" }}
-        src="https://logowik.com/content/uploads/images/nextjs2106.logowik.com.webp"
-        alt=""
+        style={{ fontFamily: 'Anjoman', fontSize: '1.5rem', color: 'blue' , width: "30px" }}
+        src="assets/img/depna.png"
+        alt="depna"
       />
+      کسرا نسل دوم
     </>
   ),
-  project: {
-    link: "https://gitlab.depna.com/s.tabatabaei/kasra-nextra-doc",
-  },
-  chat: {
-    link: "https://www.youtube.com/@JBWEBDEVELOPER",
-    icon: (
-      <>
-        <img
-          style={{ width: "35px" }}
-          src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png"
-          alt=""
-        />
-      </>
-    ),
-  },
+  // project: {
+  //   link: "https://gitlab.depna.com/s.tabatabaei/kasra-nextra-doc",
+  // },
+  // chat: {
+  //   link: "https://www.youtube.com/@JBWEBDEVELOPER",
+  //   icon: (
+  //     <>
+  //       <img
+  //         style={{ width: "35px" }}
+  //         src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png"
+  //         alt=""
+  //       />
+  //     </>
+  //   ),
+  // },
   docsRepositoryBase: "https://gitlab.depna.com/s.tabatabaei/kasra-nextra-doc",
-  footer: {
-    text: "کسرا نسل دو",
-  },
+  // footer: {
+  //   text: "هولدینگ مهیمن - محصول کسرا نسل دوم",
+  // },
 };
 
 export default config;

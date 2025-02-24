e3da8fa239ce9c0e7a87d6d8d58d94553fc257da
saber tabatabaei yazdi <saber.tabatabaee@gmail.com>
2025-02-24 13:42:35 +0330

    worked

--- kasraHelp.py
+++ kasraHelp.py
@@ -5,8 +5,18 @@ import shutil
 import requests
 import html2text
 from bs4 import BeautifulSoup
+from collections import deque
+from urllib.parse import urljoin, urlparse, unquote
+import re
+import time
 
 
+BASE_URL = "https://kasrayar.depna.com"  # پایه آدرس برای دانلود صفحات HTML
+save_directory = r"D:\saberprojects\kasra\kasra-docs\pages"
+static_img_dir = r"D:\saberprojects\kasra\kasra-docs\public\img"
+static_assets_dir = r"D:\saberprojects\kasra\kasra-docs\public\assets"
+index_file_path = os.path.join(save_directory, 'index.mdx')
+max_pages=500
 
 def update_meta_json(directory, fa_name, en_name):
     """به‌روزرسانی یا ایجاد فایل _meta.json"""
@@ -28,6 +38,199 @@ def save_content(path, content):
         f.write(content)
     print(f"💾 Saved: {os.path.relpath(path)}")
 
+def crawl_website(start_urls, save_directory, page_title):
+    urls_queue = deque([start_urls])  # Initialize the queue with the start URL
+    visited_urls = set()
+    image_extensions = ['.png', '.jpg', '.jpeg', '.gif']
+    asset_extensions = ['.zip', '.pdf', '.docx', '.xlsx', '.pptx']
+    all_links = []
+
+    print(f"Starting crawl with {len(urls_queue)} URLs in the queue.")
+    print(f"Base domain: {BASE_URL}")
+
+    while urls_queue and len(visited_urls) < max_pages:
+        current_url = urls_queue.popleft()
+
+        if current_url in visited_urls:
+            print(f"Skipping already visited URL: {current_url}")
+            continue
+
+        print(f"🔍 Processing: {current_url}")
+
+        try:
+            response = requests.get(current_url, timeout=20)
+            response.encoding = 'utf-8'
+            response.raise_for_status()
+
+            # Parse HTML
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Download and save images and assets
+            download_and_save_media(soup, current_url, static_img_dir, static_assets_dir)
+
+            # Convert to Markdown
+            # Remove duplicate text in dropspots
+            for dropspot in soup.find_all('a', class_='dropspot'):
+                open_text = dropspot.find('span', {'data-open-text': 'true'})
+                close_text = dropspot.find('span', {'data-close-text': 'true'})
+                if open_text and close_text and open_text.text == close_text.text:
+                    open_text.decompose()
+            
+            md_content = convert_to_markdown(soup)
+            md_path = create_file_path(current_url, save_directory, '.mdx')
+            add_front_matter(md_path, md_content)
+
+            print(f"Added {current_url} to visited URLs.")
+
+            # Add to index
+            relative_path = os.path.relpath(md_path, start=save_directory).replace('\\', '/')
+            link_text = os.path.splitext(os.path.basename(md_path))[0]
+            all_links.append(f'<li><a href="/docs/{relative_path}">{link_text}</a></li>')
+
+            # Extract new links
+            new_urls = extract_links(soup, current_url, BASE_URL)
+            print(f"Found {len(new_urls)} new URLs on the page.")
+
+            for url in new_urls:
+                if url not in visited_urls and url not in urls_queue and not is_media_url(url, image_extensions):
+                    if '/css/' in url.lower() or url.lower().endswith('.css'):
+                        continue
+                    urls_queue.append(url)
+                    print(f"Added URL to queue: {url}")
+
+            # Delete original HTML file if it exists
+            html_path = create_file_path(current_url, save_directory, '.html')
+            if os.path.exists(html_path):
+                os.remove(html_path)
+
+            time.sleep(1)
+
+        except Exception as e:
+            print(f"❌ Error processing {current_url}: {str(e)}")
+
+    print(f"✅ Crawling finished. {len(visited_urls)} pages processed.")
+    # Create the index file
+    create_index_page(all_links, index_file_path)
+
+
+def extract_links(soup, base_url, base_domain):
+    links = []
+    for tag in soup.find_all(['a', 'link'], href=True):
+        href = tag['href']
+        absolute_url = urljoin(base_url, href)
+        if urlparse(absolute_url).netloc == base_domain:
+            links.append(absolute_url)
+    return links
+
+
+def add_front_matter(md_path, md_content, original_title=None):
+    """اضافه کردن front matter با پشتیبانی از عنوان‌های فارسی"""
+    base_name = os.path.splitext(os.path.basename(md_path))[0]
+    
+    # استفاده از عنوان اصلی فارسی اگر موجود باشد
+    title = original_title or base_name
+    
+    front_matter = f"""---
+id: "{base_name}"
+title: "{title}"
+---
+
+"""
+    final_content = front_matter + md_content
+    save_content(md_path, final_content)
+
+
+
+def create_file_path(url, base_dir, extension):
+    """ایجاد مسیر فایل با پشتیبانی از نام‌های فارسی"""
+    parsed_url = urlparse(url)
+    path = parsed_url.path
+    path = re.sub(r'\.html?$', '', path)  # حذف پسوند .htm/.html
+    path_parts = unquote(path.strip('/')).split('/')
+    
+    # # تبدیل هر بخش مسیر به انگلیسی
+    # en_parts = []
+    # current_dir = base_dir
+    
+    # for part in path_parts:
+    #     if any(ord(c) > 127 for c in part):  # بررسی وجود کاراکتر فارسی
+    #         en_name = persian_to_english(part)
+    #         en_parts.append(en_name)
+            
+    #         # ایجاد یا به‌روزرسانی _meta.json
+    #         current_dir = os.path.join(base_dir, *en_parts[:-1])
+    #         os.makedirs(current_dir, exist_ok=True)
+    #         update_meta_json(current_dir, part, en_name)
+    #     else:
+    #         en_parts.append(part)
+    
+    # return os.path.join(base_dir, *en_parts) + extension
+
+    file_name = convert_persian_to_english(os.path.basename(path_parts[-1])) + extension
+    # فقط نام فایل را برمی‌گردانیم
+    return os.path.join(base_dir, file_name)
+
+def create_index_page(links, index_file_path):
+    index_content = """
+# فهرست
+
+## صفحات
+
+<ul>
+    {links}
+</ul>
+""".format(links='\n    '.join(links))
+    
+    with open(index_file_path, 'w', encoding='utf-8') as f:
+        f.write(index_content)
+    print(f"📄 Index page created: {index_file_path}")
+
+
+
+def download_and_save_media(soup, base_url, static_img_dir, static_assets_dir):
+    # Handle images
+    for img_tag in soup.find_all('img', src=True):
+        img_url = img_tag['src']
+        img_full_url = urljoin(base_url, img_url)
+        img_file_path = save_media_file(img_full_url, static_img_dir)
+
+        if img_file_path:
+            # Update img tag src to '/img/...' path
+            img_relative_path = '/img/' + os.path.relpath(img_file_path, static_img_dir).replace('\\', '/')
+            img_tag['src'] = img_relative_path
+    # Handle other assets (e.g., zip files)
+    for a_tag in soup.find_all('a', href=True):
+        href = a_tag['href']
+        if is_media_url(href):
+            asset_full_url = urljoin(base_url, href)
+            asset_file_path = save_media_file(asset_full_url, static_assets_dir)
+
+            if asset_file_path:
+                # Update href to '/assets/...'
+                asset_relative_path = '/assets/' + os.path.relpath(asset_file_path, static_assets_dir).replace('\\', '/')
+                a_tag['href'] = asset_relative_path
+
+def is_media_url(url, extensions =[]):
+    return any(url.lower().endswith(ext) for ext in extensions)
+
+def save_media_file(url, save_dir):
+    try:
+        parsed_url = urlparse(url)
+        media_path = os.path.join(save_dir, unquote(parsed_url.path.lstrip('/')))
+
+        response = requests.get(url, timeout=20)
+        response.raise_for_status()
+        os.makedirs(os.path.dirname(media_path), exist_ok=True)
+        with open(media_path, 'wb') as file:
+            file.write(response.content)
+        print(f"📦 Media saved: {os.path.relpath(media_path)}")
+
+        return media_path
+    except Exception as e:
+        print(f"❌ Error downloading media {url}: {str(e)}")
+        return None
+    
+
 def convert_to_markdown(soup):
     """تبدیل HTML به Markdown و حفظ کاراکترهای یونیکد"""
     converter = html2text.HTML2Text()
@@ -44,9 +247,27 @@ def convert_persian_to_english(persian_name):
         'ز': 'z', 'ژ': 'zh', 'س': 's', 'ش': 'sh', 'ص': 's', 'ض': 'z', 
         'ط': 't', 'ظ': 'z', 'ع': 'a', 'غ': 'gh', 'ف': 'f', 'ق': 'gh', 
         'ک': 'k', 'گ': 'g', 'ل': 'l', 'م': 'm', 'ن': 'n', 'و': 'v', 
-        'ه': 'h', 'ی': 'y', ' ': '_', ':': '_', '/': '_', '-': '_', '.': '_'
+        'ه': 'h', 'ی': 'y', ' ': '_', ':': '_', '/': '_', '-': '_',
+         '.': '_', 'ء': 'e' , 'ئ': 'y', ' ': '-'
     }
     return ''.join(translation_map.get(char, char) for char in persian_name)
+
+
+def persian_to_english(text):
+    """تبدیل متن فارسی به انگلیسی با استفاده از یک الگوریتم پایدار"""
+    mapping = {
+        'آ': 'a', 'ا': 'a', 'ب': 'b', 'پ': 'p', 'ت': 't', 'ث': 's',
+        'ج': 'j', 'چ': 'ch', 'ح': 'h', 'خ': 'kh', 'د': 'd', 'ذ': 'z',
+        'ر': 'r', 'ز': 'z', 'ژ': 'zh', 'س': 's', 'ش': 'sh', 'ص': 's',
+        'ض': 'z', 'ط': 't', 'ظ': 'z', 'ع': 'a', 'غ': 'gh', 'ف': 'f',
+        'ق': 'gh', 'ک': 'k', 'گ': 'g', 'ل': 'l', 'م': 'm', 'ن': 'n',
+        'و': 'v', 'ه': 'h', 'ی': 'y', 'ئ': 'y', ' ': '-', 'ء': 'e'
+    }
+    result = ''
+    for char in text:
+        result += mapping.get(char, char)
+    return result.lower().strip('-')
+
 
 def create_folder_with_meta_json(base_dir, english_name, persian_name):
     """Creates a folder and updates the _meta.json file."""
@@ -54,11 +275,11 @@ def create_folder_with_meta_json(base_dir, english_name, persian_name):
     os.makedirs(folder_path, exist_ok=True)
     
     meta_file_path = os.path.join(base_dir, '_meta.json')
+    meta_data = {}
+
     if os.path.exists(meta_file_path):
         with open(meta_file_path, 'r', encoding='utf-8') as meta_file:
             meta_data = json.load(meta_file)
-    else:
-        meta_data = {}
 
     meta_data[english_name] = persian_name
 
@@ -67,78 +288,66 @@ def create_folder_with_meta_json(base_dir, english_name, persian_name):
     
     print(f"📂 Created folder: {folder_path}")
     print(f"📄 Updated _meta.json: {meta_file_path}")
+    return folder_path  # مسیر پوشه برگشت داده شود
+
+def download_and_convert_html(page_href, save_directory, page_title):
+    page_href = page_href.replace("../contents", "")
+    """Downloads HTML page, converts to Markdown, and saves as .mdx"""
+    full_url = f"{BASE_URL}/{page_href.lstrip('../')}"  # اطمینان از مسیر صحیح
+
+    crawl_website(full_url,save_directory, page_title)
+
+
+
+def process_book_or_page(element, parent_directory):
+    """Recursively processes books and pages, creating folders and downloading content."""
+    if element.tag == "book":
+        persian_name = element.get("navtitle")
+        english_name = convert_persian_to_english(persian_name)
+
+        # Create folder for this book
+        book_path = create_folder_with_meta_json(parent_directory, english_name, persian_name)
+
+        # Process children (sub-books and pages)
+        for child in element:
+            process_book_or_page(child, book_path)
+
+    elif element.tag == "page":
+        page_title = element.get("navtitle", "")
+        page_href = element.get("href")
+
+        if page_href:
+            download_and_convert_html(page_href, parent_directory, page_title)
+
 
 def process_xml(xml_file, save_directory):
-    """Parses the XML TOC file and creates corresponding folders."""
+    """Parses the XML TOC file and processes all books and pages."""
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
     for book in root.findall("book"):
         persian_name = book.get("navtitle")
-        if persian_name == "نسل یک":
+        if persian_name == "نسل یک":  # Skipping this section
             continue
-        english_name = convert_persian_to_english(persian_name)
+        process_book_or_page(book, save_directory)
 
-        book_path = os.path.join(save_directory, english_name)
-        create_folder_with_meta_json(save_directory, english_name, persian_name)
+def remove_main_folder_and_contents(save_directory):
+    """Deletes and recreates the main folder for fresh processing."""
+    remove_all_recursive(save_directory)    
+    assets_directory = "./public/img/"
+    remove_all_recursive(assets_directory)
 
-        for sub_book in book.findall("book"):
-            sub_persian = sub_book.get("navtitle")
-            sub_english = convert_persian_to_english(sub_persian)
-
-            sub_path = os.path.join(book_path, sub_english)
-            create_folder_with_meta_json(book_path, sub_english, sub_persian)
-
-            for sub_book in book.findall("book"):
-                sub_persian = sub_book.get("navtitle")
-                sub_english = convert_persian_to_english(sub_persian)
-                sub_path = os.path.join(book_path, sub_english)
-                create_folder_with_meta_json(book_path, sub_english, sub_persian)
-
-            for page in sub_book.findall("page"):
-                page_href = page.get("href")
-                if page_href:
-                    # Download the HTML file
-                    page_href = page_href.replace("../contents", "")
-                    html_url = f"https://kasrayar.depna.com{page_href}"
-                    response = requests.get(html_url)
-                    response.raise_for_status()
-
-                    htm_file_name = os.path.basename(html_url)
-                    print(f"📄 HTML file name: {htm_file_name}")
-                    htm_path = os.path.join(book_path, sub_english)
-                    # Create the file path for saving
-                    page_english_name = convert_persian_to_english(htm_file_name)
-                    html_file_path = os.path.join(htm_path, page_english_name)
-
-                    # Save the HTML file
-                    with open(html_file_path, 'wb') as html_file:
-                        html_file.write(response.content)
-                    print(f"📄 Downloaded HTML file: {html_file_path}")
-
-                    # Convert HTML to Markdown
-                    md_content = convert_to_markdown(BeautifulSoup(response.text, 'html.parser'))
-                    md_file_path = os.path.splitext(html_file_path)[0] + ".md"
-                    save_content(md_file_path, md_content)
-
-                    # Update the _meta.json file
-                    update_meta_json(book_path, persian_name, page_english_name)
-
-def remove_main_folder_and_contents():
-    save_directory = "./pages/kasra/"
-
-    # پاک کردن محتویات پوشه
+def remove_all_recursive(save_directory):
     if os.path.exists(save_directory):
         shutil.rmtree(save_directory)
-        os.makedirs(save_directory)
-        print(f"📂 پوشه '{save_directory}' پاک شد و دوباره ایجاد گردید.")
-    return save_directory
+    os.makedirs(save_directory)
+    print(f"📂 Cleared and recreated: {save_directory}")
 
 if __name__ == "__main__":
     xml_file_path = "KasraHelp.toc.txt"
     save_directory = "./pages"
     print(f"📂 Saving to: {save_directory}")
-    save_directory = remove_main_folder_and_contents()
+    remove_main_folder_and_contents(save_directory+'/kasra')
 
     process_xml(xml_file_path, save_directory)
 

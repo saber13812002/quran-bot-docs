18631f6a01cb29b8e166c3572a9cbc8f6810d0f3
saber tabatabaei yazdi <saber.tabatabaee@gmail.com>
2025-03-02 15:46:06 +0330

    rename upper case to lower case extensions

--- rename.py
+++ rename.py
@@ -18,6 +18,16 @@ def rename_png_files_in_subdirectories(directory):
                 # Rename the file
                 os.rename(old_file_path, new_file_path)
                 print(f'Renamed: {old_file_path} to {new_file_path}')
+            # Check if the file has a .PNG extension
+            if filename.endswith('.JPG'):
+                # Create the new filename with .png extension
+                new_filename = filename[:-4] + '.jpg'
+                # Get the full path of the old and new filenames
+                old_file_path = os.path.join(root, filename)
+                new_file_path = os.path.join(root, new_filename)
+                # Rename the file
+                os.rename(old_file_path, new_file_path)
+                print(f'Renamed: {old_file_path} to {new_file_path}')
 
 # Run the function
 rename_png_files_in_subdirectories(static_img_dir)
